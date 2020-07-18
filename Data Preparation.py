fef78396ff61cd73ccf2f4e69fb6d4de4c9e6f9b
Sana-72 <52315785+Sana-72@users.noreply.github.com>
2020-07-18 19:19:37 +0530

    Update Data Preparation.py

--- Data Preparation.py
+++ Data Preparation.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
 
 import numpy as np 
 import pandas as pd 
@@ -26,54 +24,26 @@ models = keras.models
 
 print("You have TensorFlow version", tf.__version__)
 
-
-# In[2]:
-
-
 data = pd.read_csv(r"C:\Users\DOCTOR\Downloads\bbc-text.csv")
-
-
-# In[3]:
-
 
 train_size = int(len(data) * .8)
 print ("Train size: %d" % train_size)
 print ("Test size: %d" % (len(data) - train_size))
-
-
-# In[4]:
-
 
 def train_test_split(data, train_size):
     train = data[:train_size]
     test = data[train_size:]
     return train, test
 
-
-# In[5]:
-
-
 train_cat, test_cat = train_test_split(data['category'], train_size)
 train_text, test_text = train_test_split(data['text'], train_size)
-
-
-# In[6]:
-
 
 max_words = 1000
 tokenize = keras.preprocessing.text.Tokenizer(num_words=max_words,char_level=False)
 
-
-# In[7]:
-
-
 tokenize.fit_on_texts(train_text) # fit tokenizer to our training text data
 x_train = tokenize.texts_to_matrix(train_text)
 x_test = tokenize.texts_to_matrix(test_text)
-
-
-# In[8]:
-
 
 encoder = LabelEncoder()
 encoder.fit(train_cat)
@@ -81,19 +51,11 @@ y_train = encoder.transform(train_cat)
 y_test = encoder.transform(test_cat)
 
 
-# In[9]:
-
-
 num_classes = np.max(y_train) + 1
 y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
-
-
-# In[10]:
-
 
 print('x_train shape:', x_train.shape)
 print('x_test shape:', x_test.shape)
 print('y_train shape:', y_train.shape)
 print('y_test shape:', y_test.shape)
-
