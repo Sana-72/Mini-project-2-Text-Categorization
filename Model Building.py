50d52a6d090929e6e3d97c973e79667abb5284d5
Sana-72 <52315785+Sana-72@users.noreply.github.com>
2020-07-18 20:49:33 +0530

    Add files via upload

---
+++ Model Building.py
@@ -0,0 +1,42 @@
+import numpy as np 
+import pandas as pd 
+import os
+
+import itertools
+import os
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+
+from sklearn.preprocessing import LabelBinarizer, LabelEncoder
+from sklearn.metrics import confusion_matrix
+
+from tensorflow import keras
+layers = keras.layers
+models = keras.models
+
+batch_size = 32
+epochs = 2
+drop_ratio = 0.5
+
+
+model = models.Sequential()
+model.add(layers.Dense(512, input_shape=(max_words,)))
+model.add(layers.Activation('relu'))
+model.add(layers.Dense(num_classes))
+model.add(layers.Activation('softmax'))
+
+model.compile(loss='categorical_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+
+
+history = model.fit(x_train, y_train,
+                    batch_size=batch_size,
+                    epochs=epochs,
+                    verbose=1,
+                    validation_split=0.1)
+
